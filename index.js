3fac5193de3d3a875df971dde6f33c3cead1e79c
khalagai <156719570+khalagai@users.noreply.github.com>
2024-07-28 14:01:46 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 let selectedRating = 0;
 
+document.addEventListener("DOMContentLoaded", () => {
+    const ratings = document.querySelectorAll(".rate");
+    const submitButton = document.querySelector(".send button");
+
+    ratings.forEach((el, index) => {
+    el.addEventListener("click", () => handleRating(index + 1));
+    });
+
+    submitButton.addEventListener("click", handleSubmit);
+});
+
 const handleRating = (rating) => {
     selectedRating = rating;
 
     const ratings = document.querySelectorAll(".rate");
-    ratings.forEach((el)=> el.classList.remove("selected"));
+    ratings.forEach((el) => el.classList.remove("selected"));
 
     document.getElementById(`rate${rating - 1}`).classList.add(`selected`);
-  
 }
 
 const handleSubmit = () => {
@@ -15,5 +25,3 @@ const handleSubmit = () => {
     document.getElementById("main1").style.display = "block";
     document.getElementById(`selectedRating`).textContent = selectedRating;
 }
-
-
